6891aa3846d26b3e5ee63a8ef4b66a9c6a1269ca
Oleksii Shevchenko <oleksiishevchenko93@gmail.com>
2023-07-12 21:28:32 +0300

    add providers

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,24 @@
 import React, { FC } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { NavLinks } from '../constants';
+import AuthProviders from './AuthProviders';
 
 const Navbar = (): FC => {
+  const session = {};
+
+  const renderLinks = () => {
+    return NavLinks.map((link) => {
+      const { href, key, text } = link;
+
+      return (
+        <Link href={href} key={key}>
+          {text}
+        </Link>
+      )
+    });
+  };
+
   return (
     <nav className='flexBetween navbar'>
       <div className='flex-1 flexStart gap-10'>
@@ -15,8 +31,20 @@ const Navbar = (): FC => {
           />
         </Link>
         <ul className='xl:flex hidden text-small gap-7'>
-
+          {renderLinks()}
         </ul>
+      </div>
+      <div className='flexCenter gap-4'>
+        {session ? (
+         <>
+          user photo
+           <Link href={'/create-project'}>
+             Share Work
+           </Link>
+         </>
+        ) : (
+          <AuthProviders />
+        )}
       </div>
     </nav>
   );
