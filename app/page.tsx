876071cb4dc2a9748825c6b51c346ed63e1700d1
Oleksii Shevchenko <oleksiishevchenko93@gmail.com>
2023-07-12 21:04:36 +0300

    change navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+const Home = (): FC => {
+  return (
+    <section className="flexStart flex-col paddings mb-16">
+      <h1>1</h1>
+      <h1>2</h1>
+      <h1>3</h1>
+    </section>
+  );
 };
 
 export default Home;
